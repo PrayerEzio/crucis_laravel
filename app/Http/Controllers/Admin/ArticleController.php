08459871815da2adbc4cf4303ea45762c0178e25
PrayerEzio <283386295@qq.com>
2016-04-27 17:06:42 +0800

    create article table etc.

--- app/Http/Controllers/Admin/ArticleController.php
+++ app/Http/Controllers/Admin/ArticleController.php
@@ -1,8 +1,22 @@
 <?php
 
 namespace App\Http\Controllers\Admin;
+use App\Article;
+use Carbon\Carbon;
 
 class ArticleController extends CommonController
 {
-    //
+    public function index(){
+        $article = new Article();
+        $article->title = 'My frist article';
+        $article->author_id = 1;
+        $article->description = "this is article's description";
+        $article->body = "this is article's body";
+        $article->tag = 'php,laravel';
+        $article->seo_title = $article->title;
+        $article->seo_keywords = $article->tag;
+        $article->seo_ description = $article->description;
+        $article->views = 0;
+        $article->published_at = Carbon::now();
+    }
 }
---
+++ database/migrations/2016_04_27_072130_create_articles_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('author_id');
+            $table->string('title');
+            $table->text('description');
+            $table->text('body');
+            $table->text('tag');
+            $table->text('seo_title');
+            $table->text('seo_keywords');
+            $table->text('seo_ description');
+            $table->integer('views');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('article');
+    }
+}
